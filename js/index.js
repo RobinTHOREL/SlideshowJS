615c981bdec8de148d922af6d20416fe1b3e6ce5
brixton <thorelrobin@yahoo.fr>
2017-07-04 23:44:40 +0200

    Last commit, pastille dynamic

--- js/index.js
+++ js/index.js
@@ -2,13 +2,14 @@
  * Created by Kush on 13/04/2017.
  */
 
-var timeTransition = 2000;
-var timeTransitionTitle = 3000;
+var timeTransition = 2000,
+	timeTransitionTitle = 3000;
 var playDefault = false;
 var timerSlide;
 var div_rail = $("#rail");
 var img_width;
-var cpt = 0;
+var cpt = 0,
+	nbImage = 0;
 
 
 /* Mode pause au hover (+ opacity 0.7 sur l'image pour indiquer le mode pause a l'user)
@@ -50,17 +51,17 @@ function previousDisable(isDisabled) {
 	if (isDisabled) {
 		$("#previous").addClass('disabled'); //permet de setter disabled au button prev
 		$("#next").addClass('disabled'); //permet de setter disabled au button next
-		$("#pastille1").addClass('disabled');
-		$("#pastille2").addClass('disabled');
-		$("#pastille3").addClass('disabled');
-		$("#pastille4").addClass('disabled');
+
+		for (i = 0; i <= nbImage; i++)
+			$("#pastille" + i).addClass('disabled');
+
 	} else {
 		$("#previous").removeClass('disabled'); //permet de unsetter disabled au button prev
 		$("#next").removeClass('disabled'); //permet de setter disabled au button next
-		$("#pastille1").removeClass('disabled');
-		$("#pastille2").removeClass('disabled');
-		$("#pastille3").removeClass('disabled');
-		$("#pastille4").removeClass('disabled');
+
+		for (i = 0; i <= nbImage; i++)
+			$("#pastille" + i).removeClass('disabled');
+
 	}
 }
 
@@ -75,13 +76,13 @@ function nextImage() {
 		"margin-left": "-" + img_width / 10000 + "px"
 	}, timeTransition);
 	if (!$("#next").hasClass('disabled')) {
-		
+
 		previousDisable(true);
 
 		if (cpt == 0) {
 			$("#legend1").addClass('activeTitle');
 		}
-		
+
 		$(".activeTitle").each(function () {
 
 			changeTitleActiveFromMove('next', this.id);
@@ -103,7 +104,7 @@ function previousImage() {
 	if (!$("#previous").hasClass('disabled')) { //permet de test si le status est disabled sur le button prev, si il est pas disable on passe
 		previousDisable(true); // le disable
 		changeImgPrevious(); // appel maintenant pour charger img avant de se deplacer vers l'arriere , fixed bug marge blanche.
-		
+
 		$('#rail').animate({
 			"margin-left": "0px"
 		}, {
@@ -113,11 +114,11 @@ function previousImage() {
 			}
 		}); //sans changeImgPrevious mettre le mouvement la
 		//permet de enable le button quand l'anime est terminé
-		
+
 		if (cpt == 0) {
 			$("#legend1").addClass('activeTitle');
 		}
-		
+
 		$(".activeTitle").each(function () {
 			changeTitleActiveFromMove('previous', this.id);
 		});
@@ -146,7 +147,7 @@ function playPause() {
 	$("#legend").animate({
 		"margin-left": "-" + img_width / 10000 + "px"
 	}, timeTransition);
-	
+
 	if (playDefault) {
 		playDefault = false;
 		clearInterval(timerSlide);
@@ -165,326 +166,105 @@ function changePastilleActive() {
 }
 
 function changePastilleActiveFromMove(move, id) {
-	//alert('move : ' + move + ', id : ' + id);
-	switch (id) {
-	case 'pastille1':
-		if (move == 'next') {
-			$("#pastille1").prop('class', 'fa fa-circle-o pastilles');
-			$("#pastille2").addClass('active');
-			changePastilleActive();
-		} else if (move == 'previous') {
-			$("#pastille1").prop('class', 'fa fa-circle-o pastilles');
-			$("#pastille4").addClass('active');
-			changePastilleActive();
-		}
-		break;
-	case 'pastille2':
-		if (move == 'next') {
-			$("#pastille2").prop('class', 'fa fa-circle-o pastilles');
-			$("#pastille3").addClass('active');
-			changePastilleActive();
-		} else if (move == 'previous') {
-			$("#pastille2").prop('class', 'fa fa-circle-o pastilles');
-			$("#pastille1").addClass('active');
-			changePastilleActive();
-		}
-		break;
-	case 'pastille3':
-		if (move == 'next') {
-			$("#pastille3").prop('class', 'fa fa-circle-o pastilles');
-			$("#pastille4").addClass('active');
-			changePastilleActive();
-		} else if (move == 'previous') {
-			$("#pastille3").prop('class', 'fa fa-circle-o pastilles');
-			$("#pastille2").addClass('active');
-			changePastilleActive();
-		}
-		break;
-	case 'pastille4':
-		if (move == 'next') {
-			$("#pastille4").prop('class', 'fa fa-circle-o pastilles');
-			$("#pastille1").addClass('active');
-			changePastilleActive();
-		} else if (move == 'previous') {
-			$("#pastille4").prop('class', 'fa fa-circle-o pastilles');
-			$("#pastille3").addClass('active');
-			changePastilleActive();
-		}
-		break;
-	default:
-		break;
+
+	id = id.substr(-1);
+
+	if (move == 'next') {
+		$("#pastille" + id).prop('class', 'fa fa-circle-o pastilles');
+		if (id == nbImage) id = 1;
+		else id++;
+		$("#pastille" + id).addClass('active');
+		changePastilleActive();
+	} else if (move == 'previous') {
+		$("#pastille" + id).prop('class', 'fa fa-circle-o pastilles');
+		if (id == 1) id = nbImage;
+		else id--;
+		$("#pastille" + id).addClass('active');
+		changePastilleActive();
 	}
+
 }
 
 function changeTitleActiveFromMove(move, id) {
 
-	// alert('move : ' + move + ', id : ' + id );
-	switch (id) {
-	case 'legend1':
-		if (move == 'next') {
-			
-			$("h1.Title1").animate({
-				fontSize: '2.5em'
-			}, timeTransitionTitle);
-			
-			$("h3.Desc1").animate({
-				fontSize: '1.5em'
-			}, timeTransitionTitle);
-			
-			$("div.animetitle1").animate({
-				"margin-left": (img_width - ($("#legend1").width())) + "px"
-			}, timeTransitionTitle);
-			
-			$("h1.Title2").animate({
-				fontSize: '3.5em'
-			}, timeTransitionTitle);
-			
-			$("h3.Desc2").animate({
-				fontSize: '2em'
-			}, timeTransitionTitle);
-			
-			$("div.animetitle2").animate({
-				"margin-left": "-" + img_width / 10000 + "px"
-			}, timeTransitionTitle);
-			
-			$("#legend1").removeClass('activeTitle');
-			$("#legend2").prop('class', 'animetitle2');
-			$(".activeTitle").prop('class', 'animetitle2');
-			$("#legend2").addClass('activeTitle');
+	id = id.substr(-1);
 
-		} else if (move == 'previous') {
+	if (move == 'next') {
 
-			$("h1.Title1").animate({
-				fontSize: '2.5em'
-			}, timeTransitionTitle);
-			
-			$("h3.Desc1").animate({
-				fontSize: '1.5em'
-			}, timeTransitionTitle);
-			
-			$("div.animetitle1").animate({
-				"margin-left": (img_width - ($("#legend1").width())) + "px"
-			}, timeTransitionTitle);
-			
-			$("div.animetitle4").animate({
-				"margin-left": "-" + img_width / 10000 + "px"
-			}, timeTransitionTitle);
-			
-			$("h1.Title4").animate({
-				fontSize: '3.5em'
-			}, timeTransitionTitle);
-			
-			$("h3.Desc4").animate({
-				fontSize: '2em'
-			}, timeTransitionTitle);
-			
-			$("#legend1").removeClass('activeTitle');
-			$("#legend4").prop('class', 'animetitle4');
-			$(".activeTitle").prop('class', 'animetitle4');
-			$("#legend4").addClass('activeTitle');
-		}
-		break;
-	case 'legend2':
-		if (move == 'next') {
-			
-			$("h1.Title2").animate({
-				fontSize: '2.5em'
-			}, timeTransitionTitle);
-			
-			$("h3.Desc2").animate({
-				fontSize: '1.5em'
-			}, timeTransitionTitle);
-			
-			$("div.animetitle2").animate({
-				"margin-left": (img_width - ($("#legend2").width())) + "px"
-			}, timeTransitionTitle);
-			
-			$("div.animetitle3").animate({
-				"margin-left": "-" + img_width / 10000 + "px"
-			}, timeTransitionTitle);
-			
-			$("h1.Title3").animate({
-				fontSize: '3.5em'
-			}, timeTransitionTitle);
-			
-			$("h3.Desc3").animate({
-				fontSize: '2em'
-			}, timeTransitionTitle);
+		$("h1.Title" + id).animate({
+			fontSize: '2.5em'
+		}, timeTransitionTitle);
 
-			$("#legend2").removeClass('activeTitle');
-			$("#legend3").prop('class', 'animetitle3');
-			$(".activeTitle").prop('class', 'animetitle3');
-			$("#legend3").addClass('activeTitle');
+		$("h3.Desc" + id).animate({
+			fontSize: '1.5em'
+		}, timeTransitionTitle);
 
-		} else if (move == 'previous') {
+		$("div.animetitle" + id).animate({
+			"margin-left": (img_width - ($("#legend" + id).width())) + "px"
+		}, timeTransitionTitle);
 
-			$("h1.Title2").animate({
-				fontSize: '2.5em'
-			}, timeTransitionTitle);
-			
-			$("h3.Desc2").animate({
-				fontSize: '1.5em'
-			}, timeTransitionTitle);
-			
-			$("div.animetitle2").animate({
-				"margin-left": (img_width - ($("#legend2").width())) + "px"
-			}, timeTransitionTitle);
-			
-			$("div.animetitle1").animate({
-				"margin-left": "-" + img_width / 10000 + "px"
-			}, timeTransitionTitle);
+		$("#legend" + id).removeClass('activeTitle');
 
-			$("h1.Title1").animate({
-				fontSize: '3.5em'
-			}, timeTransitionTitle);
-			
-			$("h3.Desc1").animate({
-				fontSize: '2em'
-			}, timeTransitionTitle);
-			
-			$("#legend2").removeClass('activeTitle');
-			$("#legend1").prop('class', 'animetitle1');
-			$(".activeTitle").prop('class', 'animetitle1');
-			$("#legend1").addClass('activeTitle');
-		}
-		break;
-	case 'legend3':
-		if (move == 'next') {
+		if (id == nbImage) id = 1;
+		else id++;
 
-			$("h1.Title3").animate({
-				fontSize: '2.5em'
-			}, timeTransitionTitle);
-			
-			$("h3.Desc3").animate({
-				fontSize: '1.5em'
-			}, timeTransitionTitle);
-			
-			$("div.animetitle3").animate({
-				"margin-left": (img_width - ($("#legend3").width())) + "px"
-			}, timeTransitionTitle);
-			
-			$("div.animetitle4").animate({
-				"margin-left": "-" + img_width / 10000 + "px"
-			}, timeTransitionTitle);
+		$("h1.Title" + id).animate({
+			fontSize: '3.5em'
+		}, timeTransitionTitle);
 
-			$("h1.Title4").animate({
-				fontSize: '3.5em'
-			}, timeTransitionTitle);
-			
-			$("h3.Desc4").animate({
-				fontSize: '2em'
-			}, timeTransitionTitle);
-			
-			$("#legend3").removeClass('activeTitle');
-			$("#legend4").prop('class', 'animetitle4');
-			$(".activeTitle").prop('class', 'animetitle4');
-			$("#legend4").addClass('activeTitle');
+		$("h3.Desc" + id).animate({
+			fontSize: '2em'
+		}, timeTransitionTitle);
 
-		} else if (move == 'previous') {
+		$("div.animetitle" + id).animate({
+			"margin-left": "-" + img_width / 10000 + "px"
+		}, timeTransitionTitle);
 
-			$("h1.Title3").animate({
-				fontSize: '2.5em'
-			}, timeTransitionTitle);
-			
-			$("h3.Desc3").animate({
-				fontSize: '1.5em'
-			}, timeTransitionTitle);
-			
-			$("div.animetitle3").animate({
-				"margin-left": (img_width - ($("#legend3").width())) + "px"
-			}, timeTransitionTitle);
-			
-			$("div.animetitle2").animate({
-				"margin-left": "-" + img_width / 10000 + "px"
-			}, timeTransitionTitle);
-			
-			$("h1.Title2").animate({
-				fontSize: '3.5em'
-			}, timeTransitionTitle);
-			
-			$("h3.Desc2").animate({
-				fontSize: '2em'
-			}, timeTransitionTitle);
+		$("#legend" + id).prop('class', 'animetitle' + id);
+		$(".activeTitle").prop('class', 'animetitle' + id);
+		$("#legend" + id).addClass('activeTitle');
 
-			$("#legend3").removeClass('activeTitle');
-			$("#legend2").prop('class', 'animetitle2');
-			$(".activeTitle").prop('class', 'animetitle2');
-			$("#legend2").addClass('activeTitle');
-		}
-		break;
+	} else if (move == 'previous') {
 
-	case 'legend4':
-		if (move == 'next') {
-			$("h1.Title4").animate({
-				fontSize: '2.5em'
-			}, timeTransitionTitle);
-			
-			$("h3.Desc4").animate({
-				fontSize: '1.5em'
-			}, timeTransitionTitle);
-			
-			$("div.animetitle4").animate({
-				"margin-left": (img_width - ($("#legend4").width())) + "px"
-			}, timeTransitionTitle);
-			
-			$("div.animetitle1").animate({
-				"margin-left": "-" + img_width / 10000 + "px"
-			}, timeTransitionTitle);
-			
-			$("h1.Title1").animate({
-				fontSize: '3.5em'
-			}, timeTransitionTitle);
-			
-			$("h3.Desc1").animate({
-				fontSize: '2em'
-			}, timeTransitionTitle);
-			
-			$("#legend4").removeClass('activeTitle');
-			$("#legend1").prop('class', 'animetitle1');
-			$(".activeTitle").prop('class', 'animetitle1');
-			$("#legend1").addClass('activeTitle');
+		$("h1.Title" + id).animate({
+			fontSize: '2.5em'
+		}, timeTransitionTitle);
 
-		} else if (move == 'previous') {
+		$("h3.Desc" + id).animate({
+			fontSize: '1.5em'
+		}, timeTransitionTitle);
 
-			$("h1.Title4").animate({
-				fontSize: '2.5em'
-			}, timeTransitionTitle);
-			
-			$("h3.Desc4").animate({
-				fontSize: '1.5em'
-			}, timeTransitionTitle);
-			
-			$("div.animetitle4").animate({
-				"margin-left": (img_width - ($("#legend4").width())) + "px"
-			}, timeTransitionTitle);
-			
-			$("div.animetitle3").animate({
-				"margin-left": "-" + img_width / 10000 + "px"
-			}, timeTransitionTitle);
-			
-			$("h1.Title3").animate({
-				fontSize: '3.5em'
-			}, timeTransitionTitle);
-			
-			$("h3.Desc3").animate({
-				fontSize: '2em'
-			}, timeTransitionTitle);
-			
-			$("#legend4").removeClass('activeTitle');
-			$("#legend3").prop('class', 'animetitle3');
-			$(".activeTitle").prop('class', 'animetitle3');
-			$("#legend3").addClass('activeTitle');
-		}
-		break;
-	default:
-		break;
+		$("div.animetitle" + id).animate({
+			"margin-left": (img_width - ($("#legend1").width())) + "px"
+		}, timeTransitionTitle);
+
+		$("#legend" + id).removeClass('activeTitle');
+
+		if (id == 1) id = nbImage;
+		else id--;
+
+		$("div.animetitle" + id).animate({
+			"margin-left": "-" + img_width / 10000 + "px"
+		}, timeTransitionTitle);
+
+		$("h1.Title" + id).animate({
+			fontSize: '3.5em'
+		}, timeTransitionTitle);
+
+		$("h3.Desc" + id).animate({
+			fontSize: '2em'
+		}, timeTransitionTitle);
+
+		$("#legend" + id).prop('class', 'animetitle' + id);
+		$(".activeTitle").prop('class', 'animetitle' + id);
+		$("#legend" + id).addClass('activeTitle');
 	}
+
 }
 
 
 $("a").click(function () {
-	if (!$("#pastille1").hasClass('disabled') && $("#pastille2").hasClass('disabled') &&
-		$("#pastille3").hasClass('disabled') && $("#pastille4").hasClass('disabled')) {
+	if (!$("#pastille1").hasClass('disabled')) {
 		previousDisable(true);
 		$(".active").prop('class', 'fa fa-circle-o pastilles');
 		$(this).addClass('active');
@@ -508,6 +288,7 @@ function getImages() {
 					"<div id='legend" + i + "' class='animetitle" + i + "' data-id='" + i + "' ><h1 class='Title" + i + "'>" + val.title + "</h1><h3 class='Desc" + i + "'>" + val.desc + "</h3></div></div>");
 
 				$("#container").append("<a class='fa fa-circle-o pastilles' id='pastille" + i + "' onclick='getSlide(" + i + ")' aria-hidden='true'></a>");
+				nbImage++;
 
 			});
 			$("#pastille1").addClass('active');
@@ -520,32 +301,50 @@ function getImages() {
 function getSlide(destination)  { 
 
 	var source = $('.image:first').attr('data-id');
-	// alert(destination - source);
-	if (destination - source > 0)  { 
-		var count = destination - source; 
-		while (count > 0)  {
+	console.log("source: " + source + " dest : " + destination);
+
+	switch (destination) {
+	case 1:
+		if (source == nbImage) {
 			timeTransition = 1000;
 			nextImage();
 			timeTransition = 2000;
-			previousDisable(false);  
-			count--; 
-		} 
-	} 
-	else if (destination - source < 0)  { 
-		var countneg = Math.abs(destination - source);
-		if (countneg == 1) {
-			countneg += 2;
-		} else if (countneg > 2 && countneg < 4) {
-			countneg += -2;
 		}
-		while (countneg > 0)  {
+		break;
+	case nbImage:
+		if (source = 1) {
 			timeTransition = 1000;
-			nextImage();
+			previousImage();
 			timeTransition = 2000;
-			previousDisable(false); 
-			countneg--;
 		}
-	} 
+		break;
+	default:
+		if (destination - source > 0)  {
+			var count = destination - source;
+			while (count > 0)  {
+				timeTransition = 1000;
+				nextImage();
+				timeTransition = 2000;
+				previousDisable(false);
+				count--;
+			}
+		} else if (destination - source < 0)  {
+			var countneg = Math.abs(destination - source);
+			if (countneg == 1) {
+				countneg += 2;
+			} else if (countneg > 2 && countneg < 4) {
+				countneg += -2;
+			}
+			while (countneg > 0)  {
+				timeTransition = 1000;
+				previousImage();
+				timeTransition = 2000;
+				previousDisable(false);
+				countneg--;
+			}
+		}
+		break;
+	}
 }
 
 
